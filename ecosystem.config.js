efc166cbb75246f33d6bd14c2b17cdb2a6d88e9c
dynamic <kjagadale@merkleinc.com>
2019-11-30 19:24:22 +0530

    ecosystem config added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [{
         name: "weather",
         script: "./server.js",
-        max_memory_restart: '100M'
-        // instances : "5",
-        // exec_mode : "cluster"
+        max_memory_restart: '100M',
+        instances : "5",
+        exec_mode : "cluster"
     }]
 }
