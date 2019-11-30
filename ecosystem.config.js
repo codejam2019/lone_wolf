e54e232a71b37d3c5906799939f0954319e68b22
dynamic <kjagadale@merkleinc.com>
2019-11-30 17:02:41 +0530

    clustering max node upto 5 for scalability and performance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     apps: [{
         name: "weather",
-        script: "./server.js"
+        script: "./server.js",
+        max_memory_restart: '100M',
+        instances : "5",
+        exec_mode : "cluster"
     }]
 }
